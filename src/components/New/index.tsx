8e52f411eb644e9c9ba263d65415578debc2a17c
Marlon da Costa <marlon.costa@MAISTRE.LOCAL>
2019-12-05 10:58:03 -0300

    tela de lancamento de post

--- src/components/New/index.tsx
+++ src/components/New/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import {Link, withRouter, RouteComponentProps} from 'react-router-dom'
+import { Link, withRouter, RouteComponentProps } from 'react-router-dom'
+import firebase from '../../firebase'
 
-interface Props extends RouteComponentProps<any>{}
-interface State{
+import './new.css'
+interface Props extends RouteComponentProps<any> { }
+interface State {
     titulo: string,
     imagem: string,
     descricao: string
@@ -18,37 +20,56 @@ class New extends React.Component<Props, State>{
         }
         this.cadastrar = this.cadastrar.bind(this)
     }
-    cadastrar(){}
-    render(){
+    componentDidMount() {
+        if (!firebase.getCurrentUser()) {
+            this.props.history.replace('/')
+            return;
+        }
+        const { titulo, imagem, descricao } = this.state
+        if (titulo && imagem && descricao) {
+            const posts = firebase.app.ref('posts')
+            const chave = posts.push().key;
+
+
+
+        }
+    }
+    cadastrar(e: any) {
+        e.preventDefault();
+
+
+
+    }
+    render() {
         return (
             <div>
                 <header id="new">
                     <Link to="/dashboard">Voltar</Link>
                 </header>
-                <form onSubmit={this.cadastrar}>
+                <form onSubmit={this.cadastrar} id="new-form">
                     <label>Titulo</label>
-                    <input 
-                        type="text" 
-                        placeholder="Nome do post" 
-                        value={this.state.titulo} 
-                        onChange={e => this.setState({titulo: e.target.value})}
+                    <input
+                        type="text"
+                        placeholder="Nome do post"
+                        value={this.state.titulo}
+                        onChange={e => this.setState({ titulo: e.target.value })}
                         autoFocus
                     />
                     <label>Imagem</label>
-                    <input 
-                        type="text" 
-                        placeholder="Url da imagem" 
-                        value={this.state.imagem} 
-                        onChange={e => this.setState({imagem: e.target.value})}
+                    <input
+                        type="text"
+                        placeholder="Url da imagem"
+                        value={this.state.imagem}
+                        onChange={e => this.setState({ imagem: e.target.value })}
                     />
                     <label>descricao</label>
-                    <textarea 
-                        name="descricaoPost" 
-                        id="descricaoPost" 
-                        cols={30} 
-                        rows={10}                         placeholder="descrição do post" 
-                        value={this.state.descricao} 
-                        onChange={e => this.setState({descricao: e.target.value})} />
+                    <textarea
+                        name="descricaoPost"
+                        id="descricaoPost"
+                        cols={30}
+                        rows={10} placeholder="descrição do post"
+                        value={this.state.descricao}
+                        onChange={e => this.setState({ descricao: e.target.value })} />
                     <button type="submit">Cadastrar</button>
                 </form>
             </div>
